138f62cc7f86385a26f605ca6a72a937747f783e
name <piotrdab91@gmail.com>
2018-03-04 22:53:20 +0100

    second commit - for test pruposes

--- src/main/java/com/example/webApp/DemoApplication.java
+++ src/main/java/com/example/webApp/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @SpringBootApplication
 public class DemoApplication {
-	@RequestMapping("/A")
+	@RequestMapping("/B")
 	@ResponseBody
 	String home() {
 		return "Hello World!";
